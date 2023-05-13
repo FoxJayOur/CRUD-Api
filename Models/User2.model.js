ff16aa8d8bf4969ddaea5e9c820cad6eead1365b
FoxJayOur <dimacjoshryan@gmail.com>
2023-05-13 21:51:05 +0800

    add db

---
+++ Models/User2.model.js
@@ -0,0 +1,107 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const ProgramSchema = new mongoose.Schema({
+    progName: String,
+    prog: mongoose.SchemaTypes.ObjectId,
+
+});
+const Program = mongoose.model('program', ProgramSchema);
+module.exports = Program;
+
+const DepartmentSchema = new mongoose.Schema({
+    deptName: String,
+    dept: mongoose.SchemaTypes.ObjectId,
+    program: ProgramSchema
+});
+const Department = mongoose.model('department', DepartmentSchema);
+module.exports = Department;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    srcode: {
+        type: Number,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true
+    },
+    department:
+        DepartmentSchema,
+    createdAt: {
+        type: Date,
+        immutablee: true,
+        default: () => Date.now()
+    },
+    updatedAt: {
+        type: Date,
+        default: () => Date.now()
+    }
+
+});
+
+// called after saving a user
+// use function and remove => to use .this
+UserSchema.pre('save', async function (next) {
+    try{
+        const salt = await bcrypt.genSalt(12)
+        const hashedPassword = await bcrypt.hash(this.password, salt)
+        this.password = hashedPassword
+        next()
+        } catch (error) {
+        next(error)
+    }
+
+})
+
+UserSchema.methods.isValidPassword = async function (password) {
+    try{
+       return await bcrypt.compare(password, this.password)
+    } catch (error) {
+        throw error
+    }
+}
+
+
+// to create user from UserSchema
+const User2 = mongoose.model('user2', UserSchema);
+module.exports = User2;
+
+
+
+
+
+// try{
+//     Department.insertMany([
+//         {
+//             "deptName": "CICS",
+//             "program": {
+//                 "progName":  "Bachelor of Science in Computer Science (BSCS)"
+//             }  
+//         },
+//         {
+//             "deptName": "CICS",
+//             "program": {
+//                 "progName":  "Bachelor of Science in Information Technology (BS IT)"
+//             }
+//         }
+//     ])                                                                  
+
+// } catch (error){
+//     next(error);
+// }
--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -15,6 +15,7 @@ const express = require('express');
 const router = express.Router();
 const createError = require('http-errors')
 const User = require('../Models/User.model')
+const User2 = require('../Models/User2.model')
 const Data = require('../Models/Data.model')
 const Data2 = require('../Models/Data2.model')
 const Data3 = require('../Models/Data3.model')
@@ -177,11 +178,59 @@ router.post('/register', async (req, res, next) => {
 
     }
 });
+router.post('/register2', async (req, res, next) => {
+    //console.log(req.body)
+    try {
+
+        // validating req.body using authSchema (Joi)
+        const result = await authSchema.validateAsync(req.body)
+
+        const emailExist = await User2.findOne({email: result.email})
+        if (emailExist) 
+            throw createError.Conflict(result.email + ' already exists')
+    
+        // if email doesn't exist, add another user
+        const user = new User2(result)
+        const savedUser = await user.save()
+
+        const accessToken = await signAccessToken(savedUser.id)
+        const refreshToken = await signRefreshToken(savedUser.id)
+        res.send({accessToken, refreshToken})
+
+        //res.send(savedUser)
+    } catch (error){
+        // if error is comming from Joi
+        if (error.isJoi === true) error.status = 422    // 422 Unprocessable Entity
+        next(error)
+
+    }
+});
 
 router.post('/login', async (req, res, next) => {
     try {
                 // find user inside users(Collection)
         const user = await User.findOne({email: req.body.email})
+        if(user) {
+            
+        }
+        else if(!user) {throw createError.NotFound("Your credentials did not match our record.")}
+        
+        const isMatch = await user.isValidPassword(req.body.password)
+        if (!isMatch) throw createError.Unauthorized('Please ensure that your credentials are valid.')
+
+        const accessToken = await signAccessToken(user.id)
+        const refreshToken = await signRefreshToken(user.id)
+        res.send({ accessToken, refreshToken})
+        console.log({ accessToken, refreshToken})  
+        //res.send(result)
+    } catch (error){
+        next(error)
+    }
+})
+router.post('/login2', async (req, res, next) => {
+    try {
+                // find user inside users(Collection)
+        const user = await User2.findOne({email: req.body.email})
         if(user) {
             
         }
